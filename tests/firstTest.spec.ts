7b2cb0f6202e668f4f7bec927d76204cefac1eab
Richard <127592434+Rixhard5@users.noreply.github.com>
2023-12-08 16:27:18 +0100

    sub tests

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,9 +1,33 @@
 import {test} from '@playwright/test';
 
-test('the first test', async ({page}) => {
+test.beforeEach(async({page}) => {
     await page.goto('http://localhost:4200/');
+});
 
-    await page.getByText('Forms').click();
+test.describe('suite1', () => {
+    test.beforeEach(async({page}) => {
+        await page.getByText('Charts').click();
+    });
 
-    await page.getByText('Form Layouts').click();
+    test('the first test', async ({page}) => {
+        await page.getByText('Form Layouts').click();
+    });
+
+    test('navigate to datepicker page', async ({page}) => {
+        await page.getByText('Datepicker').click();
+    });
+});
+
+test.describe('suite2', () => {
+    test.beforeEach(async({page}) => {
+        await page.getByText('Forms').click();
+    });
+
+    test('the second test', async ({page}) => {
+        await page.getByText('Form Layouts').click();
+    });
+
+    test('navigate to datepicker page2', async ({page}) => {
+        await page.getByText('Datepicker').click();
+    });
 });
