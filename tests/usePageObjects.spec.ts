d51fc6408f393856ae369a502aef05181a3c28cb
Richard <127592434+Rixhard5@users.noreply.github.com>
2023-12-26 15:10:21 +0100

    Parametrized Methods

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,7 +1,8 @@
-import {expect, test} from '@playwright/test';
-import {NavigationPage} from '../page-objects/navigationPage';
+import { expect, test } from '@playwright/test';
+import { NavigationPage } from '../page-objects/navigationPage';
+import { FormLayoutsPage } from '../page-objects/formLayoutsPage';
 
-test.beforeEach(async ({page}, testInfo) => {
+test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200/');
 });
 
@@ -12,4 +13,13 @@ test('navigate to form page', async ({page}) => {
     await navigateTo.smartTablePage();
     await navigateTo.toastrPage();
     await navigateTo.tooltipPage();
+});
+
+test('parametrized methods', async ({page}) => {
+    const navigateTo = new NavigationPage(page);
+    const onFormLayoutsPage = new FormLayoutsPage(page);
+
+    await navigateTo.formLayoutsPage();
+    await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 2');
+    await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox('John Smith', 'john@test.com', false);
 });
